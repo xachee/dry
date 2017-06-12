32cbda8689f99404788dd748b2fac10819165e75
Liana <---->
2017-06-12 18:50:13 +0400

    First Commit

---
+++ app/server.js
@@ -0,0 +1,1 @@
+//koa-ejs, koa,  koa-ruter,'loover', passport,
---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+let b = new Promise(function(resolve,reject){
+    setTimeout(function(){ resolve("blabla"),0 });
+
+});
+b.then(function (data) {
+    console.log(data);
+})*/
