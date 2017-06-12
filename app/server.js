35d23e9d0e45aa88b9e5abfa9e6d73564f2f04ed
LAB1650\Administrator <xach@tumo>
2017-06-12 19:39:38 +0400

    server

--- app/server.js
+++ app/server.js
@@ -1,1 +1,11 @@
 //koa-ejs, koa,  koa-ruter,'loover', passport,
+const http=require("http");
+const koa=require("koa");
+const koaejs=require("koa-ejs");
+const KoaR=require("koa-router");
+const app= new koa();
+app.listen(8080);
+
+app.use(function (ctx) {
+    ctx.body=ctx.url;
+})
