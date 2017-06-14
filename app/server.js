ef20f0f30b88a97cf1f4c062d611e6d05469706f
LAB1651\Administrator <mhov.99@gmail.com>
2017-06-14 19:25:13 +0400

    Added playlist-page

--- app/server.js
+++ app/server.js
@@ -1,1 +1,7 @@
-//koa-ejs, koa,  koa-ruter,'loover', passport,
+"use strict"
+const http = require("http");
+const Koa = require("koa");
+const KoaEjs = require("koa-ejs");
+const router = require("koa-router");
+
+var app = new Koa();
