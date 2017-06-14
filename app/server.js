783b29d2ff55fa87ccb9e0b3af7e714c007f877c
Elina Galstyan <elina.galstyan@tumo.org>
2017-06-14 19:15:00 +0400

    Run http server from index.js

--- app/server.js
+++ app/server.js
@@ -4,7 +4,7 @@ const koa=require("koa");
 const koaejs=require("koa-ejs");
 const KoaR=require("koa-router");
 const app= new koa();
-app.listen(8080);
+
 
 app.use(function (ctx) {
     if(ctx.url=="/p1" || ctx.url=="/")
@@ -12,3 +12,5 @@ app.use(function (ctx) {
     else if(ctx.url=="/p2")
         ctx.body="<center><p>This is Page2<br><a href='p1'>Page 1</a></p></center>";
 })
+
+module.exports = app;
